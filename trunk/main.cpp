1d7e153512fd448a6cc3ccbf3eda0a70df807f73
coliver650@gmail.com <coliver650@gmail.com@e6ba64e5-74c6-6de4-ee4b-bec90eaa14f8>
2014-09-16 22:35:24 +0000

    Hello World.

---
+++ trunk/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TAG
+//
+//  Created by Christopher Oliver on 9/16/14.
+//  Copyright (c) 2014 Christopher Oliver. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
