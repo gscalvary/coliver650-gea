ba79e398cedb684e37c021adac042e00d689cc75
coliver650 <coliver650@gmail.com>
2014-09-25 16:24:00 +0000

    Updated path.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,1 @@
-Mistake that I now can't delete!  Please look under trunk/coliver650gea for the project code.
+Mistake that I now can't delete!  Please look under trunk/coliver650-gea/TAG for the text-based game project code.
