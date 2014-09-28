efd020e9ac41b50dcc2e95b1a54736d1831f863e
coliver650@gmail.com <coliver650@gmail.com@e6ba64e5-74c6-6de4-ee4b-bec90eaa14f8>
2014-09-28 21:12:46 +0000

    Updated the message, npc and point classes with function signatures and descriptions.

--- trunk/ coliver650-gea/TAG/Point.cpp
+++ trunk/ coliver650-gea/TAG/Point.cpp
@@ -1,26 +1,38 @@
 #include "Point.h"
 
+// Point:
+// EFFECT: Creates a point object in memory with x and y components both set to 0.
+// Example: see test class.
 Point::Point() {
 	x = 0;
 	y = 0;
 }
-
+// ~Point:
+// EFFECT: De-allocates the memory reserved for a Point object.
 Point::~Point() {
 
 }
-
+// getPointx: -> int
+// Takes no input but returns a number representing the x coordinate of the point in 2D space.
+// Example: see test class.
 int Point::getPointx() {
 	return x;
 }
-
+// getPointy: -> int
+// Takes no input but returns a number representing the y coordinate of the point in 2D space.
+// Example: see test class.
 int Point::getPointy() {
 	return y;
 }
-
+// updatePointx: int ->
+// Takes a number and sets the x coordinate of the point to it.
+// Example: see test class.
 void Point::updatePointx(int newx) {
 	x = newx;
 }
-
+// updatePointy: int ->
+// Takes a number and sets the y coordinate of the point to it.
+// Example: see test class.
 void Point::updatePointy(int newy) {
 	y = newy;
 }
